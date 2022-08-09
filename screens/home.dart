7a78bdfc6b967e5db233285bf7053e0e78680d46
Ahsan_Aquib_Raushan <AshleyCara@protonmail.com>
2022-08-09 10:22:09 +0530

    Added a future and stream

--- screens/home.dart
+++ screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mono_log/data_models/authservice.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mono_log/data_models/user.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -11,7 +12,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final AuthService _auth = AuthService();
+  final AuthService auth = AuthService();
+  FirebaseFirestore cloudInstance = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +34,57 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
-      body: SafeArea(child: Container()),
+      body: FutureBuilder(
+          future: cloudInstance.collection(widget.userID.uid).get(),
+          builder: (BuildContext ctx,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              if (snapshot.data!.size == 0) {
+                cloudInstance.collection(widget.userID.uid).add({
+                  "topic": "Welcome",
+                  "content":
+                      "Welcome to Mono-Logs. Here you can add, edit or delete your logs",
+                  "created-on": DateTime.now(),
+                  "colormap": {"Red": 226, "Green": 234, "Blue": 252},
+                });
+              }
+              return DocumentStream(
+                userId: widget.userID.uid,
+              );
+            } else {
+              return Container(); //add a spinkit
+              // return DocumentStream();   return a simple scaffold
+            }
+          }),
     );
+  }
+}
+
+class DocumentStream extends StatefulWidget {
+  const DocumentStream({Key? key, required this.userId}) : super(key: key);
+  final String userId;
+
+  @override
+  State<DocumentStream> createState() => _DocumentStreamState();
+}
+
+class _DocumentStreamState extends State<DocumentStream> {
+  FirebaseFirestore cloudInstance = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: cloudInstance.collection(widget.userId).snapshots(),
+        builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshots) {
+          return SafeArea(
+              child: SingleChildScrollView(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [],
+            ),
+          ));
+        });
   }
 }
